d555518eb096de770011f8506cbf534817be6975
Lauren Broski <laurenbroski@oakland.edu>
2021-12-04 18:24:59 -0500

    inital commit

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,1 +1,25 @@
-t
+app.post("/answerQ1", (req, res) => {
+    let cAnswer1 = "A";
+    let q1Answer = req.body.q1Input;
+    if (q1Answer === cAnswer1) {
+        res.send("Correct!");
+        counter++;
+    } else {
+        res.send("Incorrect");
+    }
+    console.log(q1Answer);
+    console.log(counter);
+});
+
+app.post("/answerQ2", (req, res) => {
+    let cAnswer2 = "D";
+    let q2Answer = req.body.q2Input;
+    if (q2Answer === cAnswer2) {
+        res.send("Correct!");
+        counter++;
+    } else {
+        res.send("Incorrect");
+    }
+    console.log(q2Answer);
+    //console.log(counter);
+});
