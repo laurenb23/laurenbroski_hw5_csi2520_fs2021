c43d08c188110ab4eb21d3dba2cb5e296e7dabcb
Lauren Broski <laurenbroski@oakland.edu>
2021-12-04 01:03:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ejs Middleware
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get('/my_resume', function(req, res) {
+    res.render('../views/my_resume.ejs');
+});
+
+app.get('/my_blog', function(req, res) {
+    res.render('../views/my_blog.ejs');
+});
+
+app.get('/my_funfacts', function(req, res) {
+    res.render('../views/my_fundacts.ejs');
+});
+
+app.get('/trivia', function(req, res) {
+    res.render('../views/trivia.ejs');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
