3bbd5436b214690848d2641364cfe515519768b0
Lauren Broski <laurenbroski@oakland.edu>
2021-12-04 18:40:23 -0500

    inital commit

--- index.js
+++ index.js
@@ -31,21 +31,21 @@ app.get('/my_funfacts', function(req, res) {
 app.get('/trivia', function(req, res) {
     res.render('../views/trivia.ejs');
 });
-/// 
 
-let counter = 0;
+
+let score = 0;
 
 app.post("/answerQ1", (req, res) => {
     let cAnswer1 = "A";
     let q1Answer = req.body.inputQ1;
     if (q1Answer === cAnswer1) {
         res.send("Correct!");
-        counter++;
+        score++;
     } else {
         res.send("Incorrect");
     }
     console.log(q1Answer);
-    console.log(counter);
+    console.log(score);
 });
 
 app.post("/answerQ2", (req, res) => {
@@ -53,12 +53,51 @@ app.post("/answerQ2", (req, res) => {
     let q2Answer = req.body.inputQ2;
     if (q2Answer === cAnswer2) {
         res.send("Correct!");
-        counter++;
+        score++;
     } else {
         res.send("Incorrect");
     }
     console.log(q2Answer);
-    //console.log(counter);
+    console.log(score);
+});
+
+app.post("/answerQ3", (req, res) => {
+    let cAnswer3 = "B";
+    let q3Answer = req.body.inputQ3;
+    if (q3Answer === cAnswer3) {
+        res.send("Correct!");
+        score++;
+    } else {
+        res.send("Incorrect");
+    }
+    console.log(q3Answer);
+    console.log(score);
+});
+
+app.post("/answerQ4", (req, res) => {
+    let cAnswer4 = "A";
+    let q4Answer = req.body.inputQ4;
+    if (q4Answer === cAnswer4) {
+        res.send("Correct!");
+        score++;
+    } else {
+        res.send("Incorrect");
+    }
+    console.log(q4Answer);
+    console.log(score);
+});
+
+app.post("/answerQ5", (req, res) => {
+    let cAnswer5 = "C";
+    let q5Answer = req.body.inputQ5;
+    if (q5Answer === cAnswer5) {
+        res.send("Correct!");
+        score++;
+    } else {
+        res.send("Incorrect");
+    }
+    console.log(q5Answer);
+    console.log(score);
 });
 
 const PORT = process.env.PORT || 3000;
